0227de60fd1ddd54692cf13280a1f35612aef814
Bruce Becker <brucellino@gmail.com>
2018-04-02 23:22:56 +0200

    Tests are failing for categories

--- tests/content/categories/controls/categories.rb
+++ tests/content/categories/controls/categories.rb
@@ -1,18 +1,19 @@
 # encoding: utf-8
 # copyright: 2018, The Authors
-
 title 'categories'
-#params = yaml(content: inspec.profile.file('files/server.yml')).params
-
+data = yaml(content: inspec.profile.file('categories.yml')).params
+    
 control 'category list' do
     title 'Seed Categories'
     desc 'Seed the categories with predetermined structure'
     tag 'http'
-    # describe http("#{params['base_url']}#{['categories_url']}") do
+    
     describe http('https://community.egi.eu/categories.json', method: 'GET') do
         its('status') { should cmp 200 }
         its('body') { should be }
         its('body') { should include 'category_list'}
-        its('body') { should include 'Site Feedback'}
+        data['categories'].each do |category|
+            its('body') { should include category }
+        end
     end
 end
