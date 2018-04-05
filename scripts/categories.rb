79666c65bdc04f1e3672c728ccb1f8781acf044e
Bruce Becker <brucellino@gmail.com>
2018-04-05 10:35:18 +0200

    add some category seeds

--- scripts/categories.rb
+++ scripts/categories.rb
@@ -3,8 +3,12 @@ require 'discourse_api'
 require 'awesome_print'
 require 'json'
 require 'yaml'
+
+# path to where the json seeds are
+seeds_path = '../files/forum_data/categories/'
 client = DiscourseApi::Client.new('https://community.egi.eu')
 secrets = YAML.load_file('../files/secrets.yml')
+# seed_categories = JSON.parse(File.read('../files/forum-data/categories/categories.json'))
 client.api_key = secrets['api_key']
 client.api_username = 'brucellino'
 # Create an array that will hold the parent category ids
@@ -16,9 +20,10 @@ categories.each do |category|
     if category['has_children']
         parent_categories.push(category['id'])
         # Get all the  categories with this parent_id
-    else
-        print "\n"
+        print "#{category['name']} has children \n"
     end
 end
 
-ap parent_categories
+# Get the seeds
+seed_files = Dir.glob("#{seeds_path}/*.json")
+ap seed_files
