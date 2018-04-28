8d66fa6e2903546f5d81693fdde37dc974f57307
Bruce Becker <brucellino@gmail.com>
2018-04-28 12:27:43 +0200

    Fix rubocop errors

--- scripts/categories.rb
+++ scripts/categories.rb
@@ -1,4 +1,7 @@
-#!/usr/bin/ruby
+# copyright: 2018, The Authors
+
+# encoding: UTF-8
+
 require 'discourse_api'
 require 'awesome_print'
 require 'json'
@@ -16,32 +19,27 @@ parent_categories = []
 # Get all the categories - an array of hashes
 categories = forum_client.categories
 categories.each do |category|
-  if category['has_children']
-    parent_categories.push(category['id'])
-    # Get all the  categories with this parent_id
-    print "#{category['name']} has children \n"
-  end
+  parent_categories.push(category['id']) unless category['has_children'].nil?
 end
-
 # Get the seeds
 Dir.glob("#{seeds_path}/*.yml").each do |file|
-    new_category = YAML.load_file(file)
-    new_category.each do |category|
-        print category['name'] + "\n"
-        args = { name: category['name'], 
-                        color: category['color'],
-                        text_color: category['text_color'],
-                        description: category['description'],
-                        # permissions: category['permissions'],
-                        slug: category['slug'],
-                        parent_category_id: category['parent_category_id']
-        }
-        begin
-          forum_client.create_category(args)
-        rescue => exception
-          puts "Exception #{exception}"
-        else
-          puts 'added a category'
-        end
+  new_category = YAML.load_file(file)
+  new_category.each do |category|
+    print category['name'] + "\n"
+    args = {
+      name: category['name'],
+      color: category['color'],
+      text_color: category['text_color'],
+      description: category['description'],
+      # permissions: category['permissions'],
+      slug: category['slug'],
+      parent_category_id: category['parent_category_id']
+    }
+    begin
+      forum_client.create_category(args)
+    rescue StandardError => exception
+      puts "Exception #{exception}"
     end
+    puts 'added a category'
+  end
 end
