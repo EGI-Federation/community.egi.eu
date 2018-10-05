3c618cd9cd0977681a00d362b930533c275f28e4
Bruce Becker <brucellino@gmail.com>
2018-10-05 16:37:12 +0200

    Update login feature and step defs for actual login

--- tests/cucumber/features/login.feature
+++ tests/cucumber/features/login.feature
@@ -1,5 +1,18 @@
 Feature: Log in with CheckIn A person with an identity can log in with CheckIn
+  Background: I start on the home page
+    Given I am a test user 
+    And I am on the main page
+  
   Scenario: The login button is present
     Given I visit the main page
     When I look for the login button
     Then it is present
+
+  Scenario: A test user visits the forum to log in
+    Given I click on the login button
+    Then I am taken to EGI AAI
+    Then I choose SSO
+    And I add my credentials
+    Then I am asked to release my attributes
+    Then I am taken back to the main forum
+    And I can see my profile information
--- tests/cucumber/features/step_definitions/stepdefs.rb
+++ tests/cucumber/features/step_definitions/stepdefs.rb
@@ -1,17 +1,60 @@
-require 'watir'
+require "watir"
 require "rspec/expectations"
-Given(/I visit the main page/) do
+require 'cucumber/rspec/doubles'
+
+Given('I am a test user') do
+  @username = ENV['USER_NAME']
+  @password = ENV['USER_PASSWORD']
+end
+
+Given('I am on the main page') do
   @browser = Watir::Browser.new :firefox
   @browser.goto 'https://community.egi.eu'
 end
 
-When(/I look for the login button/) do
-  @login_button = @browser.button(class: /login-button/)  
+Given("I visit the main page") do
+  @browser.goto 'https://community.egi.eu/'
+  expect(@browser.title).to match( /EGI Foundation/ )
 end
 
-Then(/it is present/) do
-  @login_button.exists?
+When("I look for the login button") do
+  @login_button = @browser.button(visible_text: /log in/i)
 end
+
+Then("it is present") do
+  expect(@login_button).to  exist
+  expect(@login_button.text).to match(/log in/i)
+end
+
+Given("I click on the login button") do
+  @login_button = @browser.button(visible_text: /log in/i)
+  @login_button.click
+end
+
+Then("I am taken to EGI AAI") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I choose SSO") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I add my credentials") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am asked to release my attributes") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am taken back to the main forum") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can see my profile information") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 
 After do
   @browser.close
